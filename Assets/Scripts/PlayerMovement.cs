91a0ed77d697ccbb4e7170900ed102e4f343c593
11lida11 <mark.furman@student.howest.be>
2017-02-22 17:11:52 +0400

    basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject PlayerBody;
+
+    private Rigidbody _playerRigid;
+	void Start ()
+	{
+	    _playerRigid = PlayerBody.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+	    {
+	        transform.Rotate(new Vector3(0,-20,0)*Time.deltaTime);
+	    }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 20, 0) * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime);
+            _playerRigid.AddForce(new Vector3(0,90,0));
+        }
+
+    }
+}
