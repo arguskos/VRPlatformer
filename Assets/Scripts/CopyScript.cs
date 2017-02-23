7cb7fb706edc6165d35fc045731e0e2a567a4856
arguskos <mark.furman@student.howest.be>
2017-02-23 13:57:03 +0400

    networking

---
+++ Assets/Scripts/CopyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyScript : Photon.MonoBehaviour {
+
+	// Use this for initialization
+    public int Index = 1;
+
+	// Update is called once per frame
+	void Update () {
+	    if (photonView.isMine)
+	    {
+	        switch (Index)
+	        {
+                case 1:
+                    transform.position = ViveManager.Instance.Head.transform.position;
+                    transform.rotation = ViveManager.Instance.Head.transform.rotation;
+
+                    break;
+	            case 2:
+                    transform.position = ViveManager.Instance.LeftHand.transform.position;
+                    transform.rotation = ViveManager.Instance.LeftHand.transform.rotation;
+
+                    break;
+                case 3:
+                    transform.position = ViveManager.Instance.RightHand.transform.position;
+                    transform.rotation = ViveManager.Instance.RightHand.transform.rotation;
+                    break;
+	                
+
+	        }
+	       
+        }
+    }
+}
---
+++ Assets/Scripts/ViveManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+
+public class ViveManager : MonoBehaviour
+{
+
+    public GameObject Head;
+    public GameObject LeftHand;
+    public GameObject RightHand;
+
+    public static ViveManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == null)
+            Instance = null;
+        
+    }
+	
+}
