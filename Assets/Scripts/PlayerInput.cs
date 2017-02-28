6bcc7b8df01fff6425e64174b05e4e154d97c7f3
Weraine <arne.wesenbeek@student.kdg.be>
2017-02-28 16:59:06 +0100

    player respawn and checkpoint edit

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,10 +9,13 @@ public class PlayerInput : MonoBehaviour {
 	public float jumpSpeed;
 	public float moveSpeed;
 	private float fallSpeed;
+    public Vector3 lastPos;
+    public GameObject papa;
 
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController> ();
+        this.gameObject.transform.parent = papa.transform;
 	}
 	
 	// Update is called once per frame
@@ -32,6 +35,17 @@ public class PlayerInput : MonoBehaviour {
 		if (Input.GetButtonDown ("Jump") && isGrounded) {
 			fallSpeed = -jumpSpeed;
 		}
+        if (Input.GetKeyUp("space") && !isGrounded)
+        {
+            //characterController.Move(Vector3.zero);
+            if (characterController.velocity.y > 0)
+            {
+                //Vector3 newVelo = new Vector3(0F, 0F - (characterController.velocity.y + jumpSpeed), 0F);
+                //characterController.Move(newVelo);
+                //characterController.velocity = Vector3.zero;
+                fallSpeed = 0;
+            }
+        }
 	}
 
 	void Fall() {
@@ -44,7 +58,22 @@ public class PlayerInput : MonoBehaviour {
 	}
 
 	void IsGrounded() {
-		isGrounded = (Physics.Raycast(transform.position, -transform.up, characterController.height/1.95F));
+        RaycastHit hit = new RaycastHit();
+		isGrounded = (Physics.Raycast(transform.position, -transform.up,out hit, characterController.height/1.95F));
 	}
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "death")
+        {
+            Quaternion rot = this.gameObject.transform.rotation;
+            Instantiate(this.gameObject, lastPos, rot);
+            Destroy(this.gameObject);
+        }
+        if (hit.gameObject.tag == "checkpoint")
+        {
+            lastPos = hit.gameObject.transform.position;
+            Destroy(hit.gameObject);
+        }
+    }
 
 }
