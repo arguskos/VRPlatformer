2a98dfe0976d33f34b87942c7da81ca1f93dfef0
arguskos <arguskos79@gmail.com>
2017-02-28 12:19:54 +0100

    swing, enemy, progress bar

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Swing")
+        {
+
+        }
+        _rigidBody.constraints = RigidbodyConstraints.None;
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            RaycastHit hit;
+            Ray r = new Ray(transform.position, -transform.up);
+            Physics.Raycast(r, out hit);
+            if (hit.collider.tag=="Enemy")
+            {
+                GameObject.Destroy(hit.collider.gameObject);
+            }
+           
+            //GameObject.Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject FinishScreen;
+    public Image ProgressBar;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //print(Mathf.Abs(Player.transform.position.x) / 11 * 100);
+        ProgressBar.GetComponent<RectTransform>().sizeDelta = new Vector2((Mathf.Abs(Player.transform.position.x)/7f*100 )/100.0f*400.0f, 10);
+
+    }
+}
