e4eeb21f2639142026550ef0898cea07391f93ab
Weraine <arne.wesenbeek@student.kdg.be>
2017-03-03 12:14:44 +0100

    finishline prefab+code

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour {
 	
@@ -7,10 +8,12 @@ public class PlayerInput : MonoBehaviour {
     public GameObject papa;
     private Renderer rend;
     public Vector3 lastPos;
+    public Text finishedtext;
 
     public float gravity;
     public float jumpSpeed;
     public float moveSpeed;
+    public float waitAfterDeathSec = 1F;
     private float fallSpeed;
     private float dPos = 0.3726866F;
     private float zSpeed = 0;
@@ -36,8 +39,6 @@ public class PlayerInput : MonoBehaviour {
         Jump();
         Move();
         die();
-        
-
     }
 
 	void Move() {
@@ -83,11 +84,7 @@ public class PlayerInput : MonoBehaviour {
         }
         if (hit.gameObject.tag == "finish")
         {
-            hit.gameObject.SendMessage("gameEnd");
-        }
-        if (hit.gameObject.tag == "ground")
-        {
-            
+            finishedtext.enabled = true;
         }
     }
     void die() 
@@ -110,7 +107,7 @@ public class PlayerInput : MonoBehaviour {
         movabele = false;
         Quaternion rot = this.gameObject.transform.rotation;
         rend.enabled  = false;
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(waitAfterDeathSec);
         Instantiate(this.gameObject, lastPos, rot);
         Destroy(this.gameObject);
     }
