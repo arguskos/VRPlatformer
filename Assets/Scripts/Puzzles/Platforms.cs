9744f7aeceadf963a590e268f3473b1626b96165
arguskos <mark.furman@student.howest.be>
2017-04-26 14:15:34 +0400

    platform 2

--- Assets/Scripts/Puzzles/Platforms.cs
+++ Assets/Scripts/Puzzles/Platforms.cs
@@ -11,7 +11,7 @@ public class Platforms : MonoBehaviour
     private bool _isGhost;
     private float _decayTimer;
     public float DecayTime=4;
-
+    public float ShakeStrength=1;
     public float ShakeTime = 3;
     private Vector3 _initPosition;
     public Transform[] _platformParts = new Transform[8];
@@ -31,12 +31,17 @@ public class Platforms : MonoBehaviour
 	        if (_decayTimer < ShakeTime)
 	        {
                 //   transform.position = new Vector3(transform.position.x+ Mathf.Sin(Time.time * 0.1f), transform.position.y+ Mathf.Sin(Time.time * 0.1f), transform.position.z+ Mathf.Sin(Time.time * 0.1f));
-                transform.position = new Vector3(_initPosition.x+ Mathf.Sin(20*Random.Range(-Time.time,Time.time))* 0.05f, _initPosition.y + Mathf.Sin(20 * Random.Range(-Time.time, Time.time)) * 0.05f, _initPosition.z + Mathf.Sin(20 * Random.Range(-Time.time, Time.time)) * 0.05f);
+                transform.position = new Vector3(_initPosition.x+ Mathf.Sin(ShakeStrength * Random.Range(-Time.time,Time.time))* 0.02f, _initPosition.y + Mathf.Sin(ShakeStrength * Random.Range(-Time.time, Time.time)) * 0.02f, _initPosition.z + Mathf.Sin(ShakeStrength * Random.Range(-Time.time, Time.time)) * 0.02f);
 
             }
             if (_decayTimer <= 0)
-	        {
-	            MakeGhost();
+            {
+                _decayTimer = 0;
+                GameObject n= Instantiate(this.gameObject);
+                n.transform.position = transform.position;
+                n.GetComponent<Platforms>().MakeGhost();    
+                
+                BreakPlatform();
 	            transform.position = _initPosition;
 	        }
         }
@@ -61,6 +66,8 @@ public class Platforms : MonoBehaviour
     public void MakeReal()
     {
         _isGhost = false;
+        _decayTimer = DecayTime;
+        GetComponent<BoxCollider>().isTrigger = false;
 
         for (int i = 0; i < _platformParts.Length; ++i)
         {
@@ -68,7 +75,60 @@ public class Platforms : MonoBehaviour
             _platformParts[i].GetComponent<Pulsating>().enabled = false;
         }
     }
+    public enum BlendMode
+    {
+        Opaque,
+        Cutout,
+        Fade,        // Old school alpha-blending mode, fresnel does not affect amount of transparency
+        Transparent // Physically plausible transparency mode, implemented as alpha pre-multiply
+    }
 
+    public void SetupMaterialWithBlendMode(Material material, BlendMode blendMode)
+    {
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                material.SetOverrideTag("RenderType", "");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = -1;
+                break;
+            case BlendMode.Cutout:
+                material.SetOverrideTag("RenderType", "TransparentCutout");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.EnableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = 2450;
+                break;
+            case BlendMode.Fade:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.EnableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = 3000;
+                break;
+            case BlendMode.Transparent:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = 3000;
+                break;
+        }
+    }
     //Enables collision with ground and changes to pulsating
     //TODO: over time decay with a transparency fade
     public void BreakPlatform()
@@ -77,7 +137,11 @@ public class Platforms : MonoBehaviour
 
         for (int i = 0; i < _platformParts.Length; ++i)
         {
-            _platformParts[i].GetComponent<Renderer>().material = GhostMat;
+            _platformParts[i].GetComponent<Renderer>().material = Mat;
+            var m = _platformParts[i].GetComponent<Renderer>().material;
+            
+            SetupMaterialWithBlendMode(m, BlendMode.Transparent);
+            m.color= new Color(m.color.r,m.color.g,m.color.b,0.5f);
             _platformParts[i].GetComponent<Pulsating>().enabled = true;
             _platformParts[i].GetComponent<Collider>().enabled = true;
             _platformParts[i].GetComponent<Rigidbody>().useGravity = true;
@@ -85,13 +149,24 @@ public class Platforms : MonoBehaviour
         }
 
         GetComponent<BoxCollider>().isTrigger = false;
-        _decayTimer = DecayTime;
+        StartCoroutine(DestroyWithTime());
+    }
+
+    public IEnumerator DestroyWithTime()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
     }
     public void MakeGhost()
     {
-        Child.GetComponent<Renderer>().material = GhostMat;
-        Child.GetComponent<Pulsating>().enabled = true;
+        _isGhost = true;
+        _clicks = 0;
         GetComponent<BoxCollider>().isTrigger = true;
 
+        for (int i = 0; i < _platformParts.Length; ++i)
+        {
+            _platformParts[i].GetComponent<Renderer>().material = GhostMat;
+            _platformParts[i].GetComponent<Pulsating>().enabled = true;
+        }
     }
 }
