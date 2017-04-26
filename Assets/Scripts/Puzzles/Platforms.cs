a7f675ba0f0fc244cc8a2a42ca6558e4ef04bcfd
WouterWeynants <wouter.weynants@hotmail.com>
2017-04-26 11:01:54 +0200

    Added platform physics on second mouseclick

--- Assets/Scripts/Puzzles/Platforms.cs
+++ Assets/Scripts/Puzzles/Platforms.cs
@@ -6,26 +6,67 @@ public class Platforms : MonoBehaviour
 {
 
     public Material Mat;
-    public GameObject Child;
+    public Material GhostMat;
     private bool _isGhost;
-	// Use this for initialization
-	void Start ()
-	{
-	    _isGhost = true;
-	}
+    private int _clicks;
+    public Transform[] _platformParts = new Transform[8];
+
+    // Use this for initialization
+    void Start()
+    {
+        _isGhost = true;
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
+    //Counts how many times this platform has been clicked
+    public void Click()
+    {
+        _clicks++;
+        if (_clicks == 1)
+        {
+            MakeReal();
+        }
+        else if (_clicks == 2)
+        {
+            BreakPlatform();
+        }
+    }
+
+    //Changes the material to visible
     public void MakeReal()
     {
         _isGhost = false;
-        Child.GetComponent<Renderer>().material = Mat;
-        Child.GetComponent<Pulsating>().enabled = false;
+
+        for (int i = 0; i < _platformParts.Length; ++i)
+        {
+            _platformParts[i].GetComponent<Renderer>().material = Mat;
+            _platformParts[i].GetComponent<Pulsating>().enabled = false;
+        }
+    }
+
+    //Enables collision with ground and changes to pulsating
+    //TODO: over time decay with a transparency fade
+    public void BreakPlatform()
+    {
+        _isGhost = true;
+
+        for (int i = 0; i < _platformParts.Length; ++i)
+        {
+            _platformParts[i].GetComponent<Renderer>().material = GhostMat;
+            _platformParts[i].GetComponent<Pulsating>().enabled = true;
+            _platformParts[i].GetComponent<Collider>().enabled = true;
+            _platformParts[i].GetComponent<Rigidbody>().useGravity = true;
+            _platformParts[i].GetComponent<PlatformPart>().IgnoreCollision = true;
+        }
+
         GetComponent<BoxCollider>().isTrigger = false;
     }
+
     //public void MakeGhost()
     //{
     //    Child.GetComponent<Renderer>().material = Mat;
